a572014c6d23561e9221c7c25f2eee41ba9c80a2
siyoon210 <siyoon210@gmail.com>
2022-01-27 18:48:19 +0900

    테스트 추가

---
+++ src/test/java/com/example/githubactiontest/TestClass.java
@@ -0,0 +1,19 @@
+package com.example.githubactiontest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass {
+
+    @Test
+    void test() {
+        // given
+
+
+        // when
+        assertTrue(true);
+
+        // then
+    }
+}
